0e9d21d872e3db418d0fb1c7f20812175056c226
Hariton Batkov <hariton.batkov@p-product.com>
2015-12-25 14:02:27 +0200

    fix for merge operation

--- LLVideoEditor/Commands/LLMergeCommand.m
+++ LLVideoEditor/Commands/LLMergeCommand.m
@@ -34,17 +34,66 @@
 }
 
 - (void)execute {
+    NSMutableArray * layerInstructions = [NSMutableArray array];
     AVMutableComposition *mixComposition = self.videoData.composition;
-    
+    CMTime duration = [self.videoData.composition duration];
+    CMTime overalDuration = duration;
     for (int i = 0; i < [self.assets count]; i++) {
         AVAsset * asset = self.assets[i];
-        AVMutableCompositionTrack *theTrack = [mixComposition addMutableTrackWithMediaType:AVMediaTypeVideo
-                                                                            preferredTrackID:kCMPersistentTrackID_Invalid];
-        [theTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, asset.duration)
-                          ofTrack:[[asset tracksWithMediaType:AVMediaTypeVideo] objectAtIndex:0]
-                           atTime:kCMTimeZero
-                            error:nil];
+        NSError *error = nil;
+        AVMutableVideoCompositionLayerInstruction *layerInstruction = nil;
+        NSArray<AVAssetTrack *> * videoTracks = [asset tracksWithMediaType:AVMediaTypeVideo];
+        NSArray<AVAssetTrack *> * audioTracks = [asset tracksWithMediaType:AVMediaTypeAudio];
+        if ([videoTracks count]) {
+            AVMutableCompositionTrack *videoTrack = [mixComposition addMutableTrackWithMediaType:AVMediaTypeVideo
+                                                                                preferredTrackID:kCMPersistentTrackID_Invalid];
+            
+            [videoTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, asset.duration)
+                                ofTrack:[videoTracks objectAtIndex:0]
+                                 atTime:overalDuration
+                                  error:&error];
+            if (error) {
+                NSLog(@"LLMergeCommand Error loading video asset %@", error);
+            }
+            layerInstruction = [AVMutableVideoCompositionLayerInstruction videoCompositionLayerInstructionWithAssetTrack:videoTrack];
+            CGAffineTransform transform = videoTrack.preferredTransform;
+            [layerInstruction setTransform:transform atTime:overalDuration];
+            [layerInstructions addObject:layerInstruction];
+        }
+        if ([audioTracks count]) {
+            AVMutableCompositionTrack *audioTrack = [mixComposition addMutableTrackWithMediaType:AVMediaTypeAudio
+                                                                                preferredTrackID:kCMPersistentTrackID_Invalid];
+            
+            [audioTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, asset.duration)
+                                ofTrack:[audioTracks objectAtIndex:0]
+                                 atTime:overalDuration
+                                  error:&error];
+            if (error) {
+                NSLog(@"LLMergeCommand Error loading audio asset %@", error);
+            }
+        }
+        overalDuration = CMTimeAdd(overalDuration, asset.duration);
+        [layerInstruction setOpacity:0.0 atTime:overalDuration];
     }
+    
+    AVMutableVideoCompositionInstruction *instruction = nil;
+    if(self.videoData.videoComposition.instructions.count == 0) {
+        instruction = [AVMutableVideoCompositionInstruction videoCompositionInstruction];
+        AVMutableVideoCompositionLayerInstruction *layerInstruction = [AVMutableVideoCompositionLayerInstruction videoCompositionLayerInstructionWithAssetTrack:self.videoData.videoCompositionTrack];
+        CGAffineTransform firstTransform = self.videoData.videoCompositionTrack.preferredTransform;
+        [layerInstruction setTransform:firstTransform atTime:kCMTimeZero];
+        [layerInstruction setOpacity:0.0 atTime:duration];
+        instruction.layerInstructions = @[layerInstruction];
+    }
+    else {
+        instruction = (AVMutableVideoCompositionInstruction *) [self.videoData.videoComposition.instructions lastObject];
+    }
+    instruction.timeRange = CMTimeRangeMake(kCMTimeZero, overalDuration);
+    
+    [layerInstructions insertObject:[instruction.layerInstructions firstObject] atIndex:0];
+    instruction.layerInstructions = layerInstructions;
+    
+    self.videoData.videoComposition.instructions = @[instruction];
 }
 
 @end
